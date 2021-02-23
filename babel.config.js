cdcdb4e4d3a9ea67949c21446e6e2b02c6ff111b
yaodebian <yaodebian@gmail.com>
2021-02-23 14:58:50 +0800

    only remove console and debugger in bundle

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,15 @@
-module.exports = {
+const babelConfig = {
   presets: [
     '@vue/cli-plugin-babel/preset'
   ],
-  plugins: [
+  plugins: []
+}
+
+if (process.env.NODE_ENV !== 'development') {
+  babelConfig.plugins = babelConfig.plugins.concat([
     'transform-remove-console',
     'transform-remove-debugger'
-  ]
+  ])
 }
+
+module.exports = babelConfig
